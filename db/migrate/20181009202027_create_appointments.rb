0ec9152772b1de707480680a8b66ab8096dd33c0
Everett <everett.bowline@gmail.com>
2018-10-09 20:21:58 +0000

    automatically backed up by learn

--- db/migrate/20181009202027_create_appointments.rb
+++ db/migrate/20181009202027_create_appointments.rb
@@ -2,8 +2,8 @@ class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
       t.datetime :appointment_datetime
-      t.references :doctor_id, index: true, foreign_key: true
-      t.references :patient_id, index: true, foreign_key: true
+      t.references :doctor, index: true, foreign_key: true
+      t.references :patient, index: true, foreign_key: true
 
       t.timestamps null: false
     end
